42cff999cc699dc28b6567162f5481df4c41e2f5
PaulBeardsley <108080617+PaulBeardsley@users.noreply.github.com>
2026-02-12 09:16:48 +0000

    Add files via upload

---
+++ vote.py
@@ -0,0 +1,10 @@
+age = input("How old are you? ")
+age = int(age)
+
+if age > 18:
+    print("You are old enough to vote.")
+    print("Use your vote wisely.")
+else:
+    print("Sorry, you cannot vote yet.")
+    print("Mind you, they're all as bad as each other.")
+print("Thank you for using Votecheck.")
